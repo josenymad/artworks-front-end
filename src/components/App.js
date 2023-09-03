4685b88ddfb6b56489ef4611491f45de9ae6b908
Jose Wills <jose.wills3@gmail.com>
2023-09-03 13:16:32 +0100

    installed react router, built navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../styles/app.css";
 import certagsLogo from "../images/certags-web.png";
+import NavBar from "./NavBar";
 
 const App = () => {
   return (
     <div className="App">
       <img src={certagsLogo} alt="Certags Logo" className="App__certags-logo" />
+      <NavBar />
     </div>
   );
 };
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/nav-bar.css";
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <ul className="navbar__list">
+        <li className="navbar__list__all-artworks">All Artworks</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;

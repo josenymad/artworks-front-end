6a4a10fce347599fb122bc41a26902689b4b4495
Jose Wills <jose.wills3@gmail.com>
2023-09-12 21:23:53 +0100

    added pdf preview, data, link and styling

--- src/components/AllArtworks.js
+++ src/components/AllArtworks.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/all-artworks.css";
 
 const AllArtworks = ({ artworks, alert }) => {
   if (artworks) {
@@ -7,11 +8,33 @@ const AllArtworks = ({ artworks, alert }) => {
         {artworks.map((artwork) => {
           return (
             <div className="all-artworks__thumbnail" key={artwork.partNumber}>
-              <img
-                src={artwork.image}
+              <object
+                data={artwork.image}
                 className="all-artworks__thumbnail-image"
                 alt="Artwork"
-              />
+                type="application/pdf"
+              >
+                {artwork.product}
+              </object>
+              <ul className="all-artworks__thumbnail-list">
+                <li className="all-artworks__thumbnail-company-product">
+                  {`${artwork.company} ${artwork.product}`}
+                </li>
+                <li className="all-artworks__thumbnail-part-number">
+                  {artwork.partNumber}
+                </li>
+                <li className="all-artworks__thumbnail-date">
+                  {new Date(artwork.date).toLocaleDateString("en-gb")}
+                </li>
+                <a
+                  href={artwork.image}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="all-artworks__thumbnail-link"
+                >
+                  Full View
+                </a>
+              </ul>
             </div>
           );
         })}

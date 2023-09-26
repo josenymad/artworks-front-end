b2e4c35552f54b151921502cfdeeda3df24b7cf3
Jose Wills <jose.wills3@gmail.com>
2023-09-26 13:46:06 +0100

    added search bar and button in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,41 @@
 import React from "react";
 import "../styles/nav-bar.css";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = () => {
   return (
     <nav className="navbar">
       <ul className="navbar__list">
-        <li className="navbar__list__all-artworks">
+        <li className="navbar__list-item">
           <Link to="/" className="navbar__list__all-artworks__link">
             All Artworks
           </Link>
         </li>
-        <li>
+        <li className="navbar__list-item">
           <Link to="add-artwork" className="navbar__list__all-artworks__link">
             Add Artwork
           </Link>
+        </li>
+        <li className="navbar__list-item">
+          <form className="navbar__list__search">
+            <label htmlFor="search">{}</label>
+            <input
+              placeholder="Company or Product code"
+              className="navbar__list__search-text"
+              type="search"
+              id="search"
+            />
+            <label htmlFor="search-button">{}</label>
+            <button
+              type="submit"
+              className="navbar__list__search-button"
+              id="search-button"
+            >
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </form>
         </li>
       </ul>
     </nav>

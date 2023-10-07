8d9d89ad43a8e643a5698af016c549b5f63c4595
Jose Wills <jose.wills3@gmail.com>
2023-10-07 13:24:24 +0100

    added conditional rendering to update artwork component

--- src/components/UpdateArtwork.js
+++ src/components/UpdateArtwork.js
@@ -2,65 +2,69 @@ import React from "react";
 import "../styles/update-artwork.css";
 
 const UpdateArtwork = ({ selectedArtwork, formData, setFormData }) => {
-  const { image } = selectedArtwork;
-  const { company, product, partNumber, date } = formData;
-  const handleInputChange = (event) => {
-    setFormData({ ...formData, [event.target.name]: event.target.value });
-  };
+  if (selectedArtwork) {
+    const { image } = selectedArtwork;
+    const { company, product, partNumber, date } = formData;
+    const handleInputChange = (event) => {
+      setFormData({ ...formData, [event.target.name]: event.target.value });
+    };
 
-  return (
-    <div className="update-artwork">
-      <object data={image} alt="Artwork" className="update-artwork__image">
-        {}
-      </object>
-      <form className="update-artwork__form">
-        <label htmlFor="company" className="update-artwork__form__label">
-          Company:{}
-        </label>
-        <input
-          id="company"
-          name="company"
-          value={company}
-          onChange={handleInputChange}
-          placeholder="Certags"
-          className="update-artwork__form__input"
-        />
-        <label htmlFor="product" className="update-artwork__form__label">
-          Product:{}
-        </label>
-        <input
-          id="product"
-          name="product"
-          value={product}
-          onChange={handleInputChange}
-          placeholder="Zip Tags"
-          className="update-artwork__form__input"
-        />
-        <label htmlFor="part-number" className="update-artwork__form__label">
-          Part Number:{}
-        </label>
-        <input
-          id="part-number"
-          name="part-number"
-          value={partNumber}
-          onChange={handleInputChange}
-          placeholder="CERUK0123"
-          className="update-artwork__form__input"
-        />
-        <label htmlFor="date" className="update-artwork__form__label">
-          Date:{}
-        </label>
-        <input
-          type="date"
-          id="date"
-          name="date"
-          value={date}
-          onChange={handleInputChange}
-          className="update-artwork__form__input"
-        />
-      </form>
-    </div>
-  );
+    return (
+      <div className="update-artwork">
+        <object data={image} alt="Artwork" className="update-artwork__image">
+          {}
+        </object>
+        <form className="update-artwork__form">
+          <label htmlFor="company" className="update-artwork__form__label">
+            Company:{}
+          </label>
+          <input
+            id="company"
+            name="company"
+            value={company}
+            onChange={handleInputChange}
+            placeholder="Certags"
+            className="update-artwork__form__input"
+          />
+          <label htmlFor="product" className="update-artwork__form__label">
+            Product:{}
+          </label>
+          <input
+            id="product"
+            name="product"
+            value={product}
+            onChange={handleInputChange}
+            placeholder="Zip Tags"
+            className="update-artwork__form__input"
+          />
+          <label htmlFor="part-number" className="update-artwork__form__label">
+            Part Number:{}
+          </label>
+          <input
+            id="part-number"
+            name="part-number"
+            value={partNumber}
+            onChange={handleInputChange}
+            placeholder="CERUK0123"
+            className="update-artwork__form__input"
+          />
+          <label htmlFor="date" className="update-artwork__form__label">
+            Date:{}
+          </label>
+          <input
+            type="date"
+            id="date"
+            name="date"
+            value={date}
+            onChange={handleInputChange}
+            className="update-artwork__form__input"
+          />
+        </form>
+      </div>
+    );
+  }
+
+  return null;
 };
 
 export default UpdateArtwork;
